d5d5f92f1a1f3c4eaff7eb318ba7adb02d3e6706
Leon Gell <lgell@lcs.on.ca>
2023-02-06 20:25:09 -0500

    Initial Commit

---
+++ ConcreteCalc/ConcreteCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcreteCalcApp.swift
+//  ConcreteCalc
+//
+//  Created by Leon Gell on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct ConcreteCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
