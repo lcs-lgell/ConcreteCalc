7707070e04ddb96a84183cb1a955b6833016abdb
Leon Gell <lgell@lcs.on.ca>
2023-02-07 20:42:20 -0500

    working on getting precision

--- ConcreteCalc/Model/ModelView.swift
+++ ConcreteCalc/Model/ModelView.swift
@@ -12,60 +12,89 @@ struct ModelView: View {
     @State var height: String = ""
     @State var width: String = ""
     @State var length: String = ""
+    @State var desiredPrecision: Double = 2
+    let bagdistr = 0.375
     
     //MARK: Computed Properties
     //convert height
     var heightAsOptionalDouble: Double? {
         guard let unwrappedheight = Double(height) else {
-                    // Could not unwrap – invalid input
-                    return nil
-                }
-
-                // Return the unwrapped value
-                return unwrappedheight
+            // Could not unwrap – invalid input
+            return nil
+        }
+        return unwrappedheight
     }
+        
+                
     //convert height
     var widthAsOptionalDouble: Double? {
         guard let unwrappedwidth = Double(width) else {
                     // Could not unwrap – invalid input
                     return nil
                 }
-
-                // Return the unwrapped value
-                return unwrappedwidth
+        return unwrappedwidth
     }
-    //convertlength
+                
+    //convert length
     var lengthAsOptionalDouble: Double? {
         guard let unwrappedlength = Double(length) else {
                     // Could not unwrap – invalid input
                     return nil
                 }
-
-                // Return the unwrapped value
-                return unwrappedlength
+        return unwrappedlength
     }
     
+    var area: Double? {
+        guard let height = heightAsOptionalDouble, let width = widthAsOptionalDouble, let length = lengthAsOptionalDouble else {
+            return nil
+        }
+        let volume = height * width * length
+        return volume
+    }
+    
+    let bagsNeeded = (area * 0.000578704)
+    
+    
+    
+    
     var body: some View {
-        VStack {
+        ScrollView {
+            //Image which will be variable
             Image("Mortar")
                 .resizable()
                 .scaledToFit()
                 .padding()
+            
+        // Test to explain user it is in inches
+            Text("*Measurements in inches")
+                .font(.title2)
+            
             HStack(spacing: -10) {
                 Text("Height:")
+                
             TextField("25.0", text: $height)
                 .padding(.horizontal)
                 .font(.title2)
                 Text("length:")
+                
             TextField("25.0", text: $length)
                 .padding(.horizontal)
                 .font(.title2)
                 Text("Weight:")
+                
             TextField("25.0", text: $width)
                 .padding(.horizontal)
                 .font(.title2)
             }
             .padding()
+            //image for box
+            Image("Rectangle")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            //write out anwsers for area, amount of bags, and estimated price
+            Text("Volume: \(area ?? 0 .formatted(.number.precision(.significantDigits(Int(desiredPrecision)))))")
+            Text("")
                
             
         }
